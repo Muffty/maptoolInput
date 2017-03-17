51aec1a8700d87b23d1ec61679f3190492e8f8c6
Tobias <tobbes89@bitfighters.com>
2017-03-17 18:01:48 +0100

    dataReader

--- app/src/main/java/com/bitfighters/maptool/maptoolinput/MyData.java
+++ app/src/main/java/com/bitfighters/maptool/maptoolinput/MyData.java
@@ -1,7 +1,10 @@
 package com.bitfighters.maptool.maptoolinput;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 
 import net.rptools.lib.MD5Key;
 import net.rptools.maptool.model.AndroidCampaign;
@@ -10,11 +13,18 @@ import net.rptools.maptool.model.AndroidZone;
 import net.rptools.maptool.model.Asset;
 import net.rptools.maptool.model.GUID;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+
+import static android.R.attr.path;
 
 public class MyData {
 
@@ -34,12 +44,15 @@ public class MyData {
 
 	private HashMap<GUID, List<GUID>> visionMaps;
 
+	private LocalFileReader fileReader;
+
 	public MyData(String myTokenName){
 		instance = this;
 		this.myTokenName = myTokenName;
 		bitmaps = new HashMap<MD5Key, Bitmap>();
 		bitmapsLoading = new HashSet<>();
         visionMaps = new HashMap<>();
+		fileReader = new LocalFileReader();
 	}
 
 	public void initiateCampaign(AndroidCampaign campaign){
@@ -185,6 +198,17 @@ public class MyData {
 	public Bitmap getBitmap(MD5Key key) {
 		return bitmaps.get(key);
 	}
+
+	public Bitmap loadLocalCashBitmap(MD5Key key){
+		String file = key.toString();
+		if(fileReader.hasFile(file)){
+			Bitmap bmp = fileReader.readFileAsBitmap(file);
+			bitmaps.put(key, bmp);
+			return bmp;
+		}
+		return null;
+	}
+
 	public Bitmap getBitmapOrDefault(MD5Key key, Bitmap defaultMap) {
 		Bitmap map = getBitmap(key);
 		if(map == null)
@@ -245,6 +269,59 @@ class Position{
 	public void update(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+
+}
+class LocalFileReader{
+
+	public boolean isExternalStorageWritable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean hasFile(String filename) {
+	}
+
+	public Bitmap readFileAsBitmap(String filename){
+		ContextWrapper cw = new ContextWrapper(MainTab.instance.getBaseContext());
+
+		String path = cw.getDir("imageDir", Context.MODE_PRIVATE).getAbsolutePath();
+
+		try {
+			File f=new File(path, filename+".jpg");
+			Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+			return b;
+		}
+		catch (FileNotFoundException e)
+		{
+			return null;
+		}
+	}
+
+	public void saveToInternalStorage(String filename, Bitmap bitmapImage){
+		ContextWrapper cw = new ContextWrapper(MainTab.instance.getBaseContext());
+		// path to /data/data/yourapp/app_data/imageDir
+		File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+		// Create imageDir
+		File mypath=new File(directory,filename+".jpg");
+
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(mypath);
+			// Use the compress method on the BitMap object to write image to the OutputStream
+			bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
